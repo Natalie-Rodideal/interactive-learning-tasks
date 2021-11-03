cf0517ecd59b8a0bdd5cad6b5b7b1e7d43d0b6e2
natalie-rodideal <natalie.rodideal@gmail.com>
2021-11-03 20:44:07 +0000

    group

--- iam/iam.tf
+++ iam/iam.tf
@@ -1,14 +1,6 @@
 resource "aws_iam_user" "bob" {
   name = "bob"
-}
-
-resource "aws_iam_user" "multiuser" {
-  name = each.key
-  for_each = toset([
-    "bob",
-  ])
-
   tags = {
-    tag-key = "Team=DevOps"
+    Team="DevOps"
   }
 }
--- iam/iam_group.tf
+++ iam/iam_group.tf
@@ -1,12 +1,4 @@
 resource "aws_iam_group" "sysusers" {
   name = "sysusers"
-}
-
-resource "aws_iam_group" "sysuser" {
-  name = each.key
-  for_each = toset([
-    "IAM",
-    
-  ])
-
+  
 }
