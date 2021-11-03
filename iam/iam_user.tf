2193873b3eb9270f36734e1674a96aaead3f324b
natalie-rodideal <natalie.rodideal@gmail.com>
2021-11-03 21:15:57 +0000

    added

--- iam/iam_user.tf
+++ iam/iam_user.tf
@@ -3,17 +3,3 @@ resource "aws_iam_user" "bob" {
   tags = {
     Team="DevOps"
   }
-}
-resource "aws_iam_group" "sysusers" {
-  name = "sysusers"
-}
-
-resource "aws_iam_user_group_membership" "sysusersgroup" {
-  user = aws_iam_user.bob.name
-  groups = [
-    
-    aws_iam_group.sysusers.name,
-    
-  ]
-
-}
